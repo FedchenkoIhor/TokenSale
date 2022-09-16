d84b527285a1989e07b8dc2e0749f81fb10705ae
IhorFedchenko <if.ihorfedchenko@gmail.com>
2022-09-16 16:58:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fedchenkoihor.github.io
---
+++ js/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// SET CURRENT YEAR IN FOOTER
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// STICKY NAVIGATION
+const sectionHeroEl = document.querySelector("header");
+const btnMobileMenu = document.querySelector(".btn__mobile-menu");
+const menuItems = document.querySelector(".menu-items");
+const btnShowHidde = (elem, event) => {
+  elem.style.display = event;
+};
+
+const obs = new IntersectionObserver(
+  (entiers) => {
+    const ent = entiers[0];
+    if (screen.width > 928) {
+      if (!ent.isIntersecting) {
+        btnShowHidde(btnMobileMenu, "inline-flex");
+        menuItems.classList.add("mobile");
+      } else {
+        btnShowHidde(btnMobileMenu, "none");
+        menuItems.classList.remove("mobile");
+      }
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// OPEN / CLOSE MOBILE MENU
+btnMobileMenu.addEventListener("click", () => {
+  btnMobileMenu.classList.toggle("open");
+  if (btnMobileMenu.classList.contains("open")) {
+    btnMobileMenu.innerHTML = "&larr;";
+    menuItems.style.left = "0";
+  } else {
+    btnMobileMenu.innerHTML = "&rarr;";
+    menuItems.style.left = "-100%";
+  }
+});
+
+// CONTROL CURRENT SCREEN SIZE
+window.addEventListener("resize", () => {
+  if (screen.width <= 928) {
+    btnShowHidde(btnMobileMenu, "inline-flex");
+    menuItems.classList.add("mobile");
+  } else {
+    btnShowHidde(btnMobileMenu, "none");
+    menuItems.classList.remove("mobile");
+  }
+});
