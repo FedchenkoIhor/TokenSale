b28d76feb81c867550631024db2324415f58ba6f
IhorFedchenko <if.ihorfedchenko@gmail.com>
2022-09-29 10:44:29 +0300

    add grids video

--- js/script.js
+++ js/script.js
@@ -183,8 +183,4 @@ const swiper = new Swiper(".mySwiper", {
       slidesPerView: 2,
     },
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
